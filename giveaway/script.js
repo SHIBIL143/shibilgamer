f29475f75cb2624a7b8d0e98d4161fca562565c2
SHIBIL143 <103309836+SHIBIL143@users.noreply.github.com>
2024-08-09 07:03:35 +0530

    Update script.js

--- giveaway/script.js
+++ giveaway/script.js
@@ -8,16 +8,16 @@ document.getElementById('giveaway-form').addEventListener('submit', function(eve
     const botToken = '6730696754:AAFj8glN-U2dMMeZ80BONllbgNumfK8_qeg';
     const channelId = '@SG_BROADCAST';  // Use '@' before your channel ID
 
-    // Use MarkdownV2 for bold text
-    const message = `*New Member Participated Giveaway*\n\n*Name:* ${fullname}\n*Telegram Username:*@ ${telegramUsername}`;
-    const formattedMessage = `*New\\ Member\\ Participated\\ Giveaway*\n\n*Name:*\\ ${fullname}\n*Telegram\\ Username:*@\\ ${telegramUsername}`;
+    // Format the message with bold text using MarkdownV2
+    const formattedMessage = `*New Member Participated Giveaway*\n\n*Name:* ${fullname}\n*Telegram Username:* ${telegramUsername}`;
+
     const participateLink = 'https://shibilgamer.online/giveaway';
 
     // Prepare the payload with the message and inline button
     const payload = {
         chat_id: channelId,
         text: formattedMessage,
-        parse_mode: 'MarkdownV2',  // Use MarkdownV2 for stricter formatting
+        parse_mode: 'MarkdownV2',  // Use MarkdownV2 for strict formatting
         reply_markup: {
             inline_keyboard: [[
                 { text: 'Link to participate', url: participateLink }

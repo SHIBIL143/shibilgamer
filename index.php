143708e53a3e3e64b4ffa90e928724af3c65169b
SHIBIL143 <103309836+SHIBIL143@users.noreply.github.com>
2024-08-09 23:15:10 +0530

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$isMobile = preg_match('/(android|iphone|ipad|mobile)/i', $_SERVER['HTTP_USER_AGENT']);
-if ($isMobile) {
-    header('Location: https://m.shibilgamer.online');
-    exit;
-}
-?>
